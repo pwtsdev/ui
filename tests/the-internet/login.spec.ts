09f54f26236599499cd1750dcad31c59a4ebc550
bkita <2436425+bkita@users.noreply.github.com>
2024-10-23 12:53:48 +0200

    test: add login test

---
+++ tests/the-internet/login.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('The Internet - login page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await expect(page).toHaveTitle(/Internet/);
+  });
+
+  test('login with valid credentials', async ({ page }) => {
+    await page.locator('#username').fill('tomsmith');
+    await page.locator('#password').fill('SuperSecretPassword!');
+    await page.getByText('Login').last().click();
+
+    await expect(page.getByText('You logged into a secure area!')).toBeVisible();
+  });
+});
