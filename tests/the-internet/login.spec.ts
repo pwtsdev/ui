91c95ff4915b25304b20b41be62e4ed0aad31c46
bkita <2436425+bkita@users.noreply.github.com>
2024-10-23 13:47:27 +0200

    test: add new, recorded, test

--- tests/the-internet/login.spec.ts
+++ tests/the-internet/login.spec.ts
@@ -13,4 +13,12 @@ test.describe('The Internet - login page', () => {
 
     await expect(page.getByText('You logged into a secure area!')).toBeVisible();
   });
+
+  test('login with valid credentials - record', async ({ page }) => {
+    await page.getByLabel('Username').fill('tomsmith');
+    await page.getByLabel('Password').fill('SuperSecretPassword!');
+    await page.getByRole('button', { name: ' Login' }).click();
+
+    await expect(page.getByRole('heading', { name: 'Welcome to the Secure Area.' })).toBeVisible();
+  });
 });
