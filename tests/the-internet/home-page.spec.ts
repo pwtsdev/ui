bbf5019aeb105a05afd85e3e24888a182b89e4a0
bkita <2436425+bkita@users.noreply.github.com>
2024-10-23 12:19:36 +0200

    test: the internet - add first test

---
+++ tests/the-internet/home-page.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('The Internet - home page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    await expect(page).toHaveTitle(/Internet/);
+  });
+
+  test('page has correct title', async ({ page }) => {
+    await expect(page.getByText('Welcome to the-internet')).toBeVisible();
+  });
+
+  test('page has clickable links', async ({ page }) => {
+    await page.getByText('Form Authentication').click();
+    await expect(page.getByText('Login Page')).toBeVisible();
+  });
+});
