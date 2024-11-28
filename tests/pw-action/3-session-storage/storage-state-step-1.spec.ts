616b6efec87d92c9bc80a7903ed053cfbe9b6a4a
bkita <2436425+bkita@users.noreply.github.com>
2024-11-28 13:54:21 +0100

    test: new session storage test with beforeAll and afterAll

--- tests/pw-action/3-session-storage/storage-state-step-1.spec.ts
+++ tests/pw-action/3-session-storage/storage-state-step-1.spec.ts
@@ -4,7 +4,6 @@ import { ADMIN_LOGIN_SESSION, USER_LOGIN_SESSION } from '../../../playwright.con
 test.describe.serial('Advanced examples', () => {
   test('save storage state - admin', { tag: '@session' }, async ({ page }) => {
     await page.goto('https://pwts.dev/examples/ui/session/index.php');
-
     await page.fill('#username', 'admin');
     await page.fill('#password', 'admin123');
     await page.getByRole('button', { name: 'Zaloguj się' }).click();
@@ -19,20 +18,18 @@ test.describe.serial('Advanced examples', () => {
     const page = await context.newPage();
 
     await page.goto('https://pwts.dev/examples/ui/session/books.php');
-
     await expect(page.getByText('Jesteś zalogowany jako admin')).toBeVisible();
   });
 
   test('save storage state - user', { tag: '@session' }, async ({ page }) => {
     await page.goto('https://pwts.dev/examples/ui/session/index.php');
-
     await page.fill('#username', 'user');
     await page.fill('#password', 'user123');
     await page.getByRole('button', { name: 'Zaloguj się' }).click();
 
     await page.context().storageState({ path: USER_LOGIN_SESSION });
 
-    await expect(page.getByText('Jesteś zalogowany jako użytkownik')).toBeVisible();
+    await expect(page.getByText('Jesteś zalogowany jako użytkownik: user')).toBeVisible();
   });
 
   test('load storage state - user', { tag: '@session' }, async ({ browser }) => {
@@ -40,7 +37,6 @@ test.describe.serial('Advanced examples', () => {
     const page = await context.newPage();
 
     await page.goto('https://pwts.dev/examples/ui/session/books.php');
-
-    await expect(page.getByText('Jesteś zalogowany jako użytkownik')).toBeVisible();
+    await expect(page.getByText('Jesteś zalogowany jako użytkownik: user')).toBeVisible();
   });
 });
